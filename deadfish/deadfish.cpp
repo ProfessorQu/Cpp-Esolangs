73be695b150be9f4fd447bbfcdfd2ab01f3873b4
ProfessorQu <quint@dasookwat.com>
2022-07-10 19:02:38 +0200

    Created deadfish programming language interpreter

---
+++ deadfish/deadfish.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+    int accumulator = 0;
+
+    std::fstream file;
+    file.open(argv[1]);
+
+    if (!file.is_open())
+    {
+        std::cout << "File not found" << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        for (int i = 0; i < line.length(); i++)
+        {
+            // Increment
+            if (line[i] == 'i')
+            {
+                accumulator++;
+            }
+            // Decrement
+            else if (line[i] == 'd')
+            {
+                accumulator--;
+            }
+            // Square
+            else if (line[i] == 's')
+            {
+                accumulator *= accumulator;
+            }
+            // Print
+            else if (line[i] == 'o')
+            {
+                std::cout << accumulator << std::endl;
+            }
+
+            if (accumulator < 0 || accumulator == 256)
+            {
+                accumulator = 0;
+            }
+        }
+    }
+
+    return 0;
+}
