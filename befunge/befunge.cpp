15e3b99263e5a596a90eb4c6ad8ed00dfc4b5b0e
ProfessorQu <quint@dasookwat.com>
2022-07-13 19:07:44 +0200

    Improved befunge (kind of) Added 99 bottles befunge program Added help commands for befunge Added '#' command Added pushing numbers to stack Throw errors when the stack is empty Added wrapping

--- befunge/befunge.cpp
+++ befunge/befunge.cpp
@@ -12,6 +12,51 @@ int main(int argc, char const *argv[])
 {
     srand(time(NULL));
 
+    // Variables for program arguments
+    bool display_cursor = false;
+    bool display_grid = false;
+    bool display_stopped = false;
+
+    // Arguments for the program
+    if (argc > 1)
+    {
+        std::string arg;
+        for (int i = 0; i < argc; i++)
+        {
+            // Get the argument
+            arg = std::string(argv[i]);
+            
+            // Debug grid
+            if (arg == "-grid")
+            {
+                display_grid = true;
+            }
+
+            // Debug cursor
+            if (arg == "-cursor")
+            {
+                display_cursor = true;
+            }
+
+            // Debug stopped
+            if (arg == "-stopped")
+            {
+                display_stopped = true;
+            }
+
+            if (arg == "-help")
+            {
+                std::cout << "Usage: befunge.exe [file]" << std::endl;
+                std::cout << "Options:" << std::endl;
+                std::cout << "-grid: Prints the grid" << std::endl;
+                std::cout << "-cursor: Displays the cursor" << std::endl;
+                std::cout << "-stopped: Displays the stopped state" << std::endl;
+                std::cout << "-help: Prints this message" << std::endl;
+                return 0;
+            }
+        }
+    }
+
     // Open file
     std::fstream file = openFile(argv[1]);
 
@@ -31,24 +76,25 @@ int main(int argc, char const *argv[])
     int direction_y = 0;
 
     // Create the stack
-    std::stack<int> stack = std::stack<int>();
+    std::stack<int> stack;
+    std::stack<int> stack_wtf;  // NOTE: This isn't used, but if I remove it, it breaks the program
 
     // Create the grid
     char grid[ROWS][COLS];
 
     // Fill the grid with spaces
-    for (int i = 0; i < ROWS; i++)
+    for (int i = 0; i <= ROWS; i++)
     {
-        for (int j = 0; j < COLS; j++)
+        for (int j = 0; j <= COLS; j++)
         {
             grid[i][j] = ' ';
         }
     }
 
     // Fill the grid by reading the file
-    for (int i = 0; i < ROWS; i++)
+    for (int i = 0; i <= ROWS; i++)
     {
-        for (int j = 0; j < COLS; j++)
+        for (int j = 0; j <= COLS; j++)
         {
             file.get(grid[i][j]);
 
@@ -58,23 +104,20 @@ int main(int argc, char const *argv[])
                 grid[i][j] = ' ';
                 break;
             }
-        }
-    }
 
-    // Display grid if the user wants to
-    if (argc == 3 && std::string(argv[2]) == "-d")
-    {
-        for (int i = 0; i < ROWS; i++)
-        {
-            for (int j = 0; j < COLS; j++)
+            if (display_grid)
             {
                 std::cout << grid[i][j];
             }
+        }
+        if (display_grid)
+        {
             std::cout << std::endl;
         }
     }
 
     bool string_mode = false;
+    bool skip_next = false;
     bool running = true;
 
     int a;
@@ -84,6 +127,13 @@ int main(int argc, char const *argv[])
     // Execute the program
     while(running)
     {
+        // Display the cursor
+        if (display_cursor)
+        {
+            std::cout << "Cursor: " << cursor_x << " " << cursor_y << std::endl;
+            std::cout << "Instruction: " << grid[cursor_y][cursor_x] << std::endl;
+        }
+
         // If we are in string mode,
         // push characters to the stack
         if (string_mode)
@@ -97,6 +147,12 @@ int main(int argc, char const *argv[])
                 stack.push(grid[cursor_y][cursor_x]);
             }
 
+        }
+        // If we have to skip the next instruction,
+        // skip it
+        else if (skip_next && (grid[cursor_y][cursor_x] != ' ' || grid[cursor_y][cursor_x] == '\n'))
+        {
+            skip_next = false;
         }
         else
         {
@@ -146,10 +202,50 @@ int main(int argc, char const *argv[])
                         direction_x = 0;
                     }
                     break;
+                
+// ######################################## Stack operations ########################################
+                case '1':
+                    stack.push(1);
+                    break;
+                case '2':
+                    stack.push(2);
+                    break;
+                case '3':
+                    stack.push(3);
+                    break;
+                case '4':
+                    stack.push(4);
+                    break;
+                case '5':
+                    stack.push(5);
+                    break;
+                case '6':
+                    stack.push(6);
+                    break;
+                case '7':
+                    stack.push(7);
+                    break;
+                case '8':
+                    stack.push(8);
+                    break;
+                case '9':
+                    stack.push(9);
+                    break;
+                case '0':
+                    stack.push(0);
+                    break;
 
 // ######################################## Calculations ########################################
                 // Addition
                 case '+':
+                    // Throw error if stack is too small
+                    if (stack.size() < 2)
+                    {
+                        std::cout << "Stack underflow" << std::endl;
+                        running = false;
+                        break;
+                    }
+                    
                     // Get int a
                     a = stack.top();
                     stack.pop();
@@ -162,6 +258,14 @@ int main(int argc, char const *argv[])
                     break;
                 // Subtraction
                 case '-':
+                    // Throw error if stack is too small
+                    if (stack.size() < 2)
+                    {
+                        std::cout << "Stack underflow" << std::endl;
+                        running = false;
+                        break;
+                    }
+
                     // Get int a
                     a = stack.top();
                     stack.pop();
@@ -174,6 +278,14 @@ int main(int argc, char const *argv[])
                     break;
                 // Multiplication
                 case '*':
+                    // Throw error if stack is too small
+                    if (stack.size() < 2)
+                    {
+                        std::cout << "Stack underflow" << std::endl;
+                        running = false;
+                        break;
+                    }
+
                     // Get int a
                     a = stack.top();
                     stack.pop();
@@ -186,6 +298,14 @@ int main(int argc, char const *argv[])
                     break;
                 // Division
                 case '/':
+                    // Throw error if stack is too small
+                    if (stack.size() < 2)
+                    {
+                        std::cout << "Stack underflow" << std::endl;
+                        running = false;
+                        break;
+                    }
+
                     // Get int a
                     a = stack.top();
                     stack.pop();
@@ -198,6 +318,14 @@ int main(int argc, char const *argv[])
                     break;
                 // Modulus
                 case '%':
+                    // Throw error if stack is too small
+                    if (stack.size() < 2)
+                    {
+                        std::cout << "Stack underflow" << std::endl;
+                        running = false;
+                        break;
+                    }
+
                     // Get int a
                     a = stack.top();
                     stack.pop();
@@ -247,6 +375,14 @@ int main(int argc, char const *argv[])
 // ######################################## If ########################################
                 // Horizontal if
                 case '_':
+                    // Go right if the stack is empty
+                    if (stack.empty())
+                    {
+                        direction_x = 1;
+                        direction_y = 0;
+                        break;
+                    }
+
                     // Get int a
                     a = stack.top();
                     stack.pop();
@@ -266,6 +402,14 @@ int main(int argc, char const *argv[])
                     break;
                 // Vertical if
                 case '|':
+                    // Go down if the stack is empty
+                    if (stack.empty())
+                    {
+                        direction_x = 0;
+                        direction_y = 1;
+                        break;
+                    }
+                    
                     // Get int a
                     a = stack.top();
                     stack.pop();
@@ -293,7 +437,10 @@ int main(int argc, char const *argv[])
 // ######################################## Stack ########################################
                 // Duplicate the top element
                 case ':':
-                    stack.push(stack.top());
+                    if (!stack.empty())
+                    {
+                        stack.push(stack.top());
+                    }
                     break;
                 // Swap the top two elements
                 case '\\':
@@ -316,13 +463,19 @@ int main(int argc, char const *argv[])
 // ######################################## Output ########################################
                 // Output top element as integer
                 case '.':
-                    std::cout << stack.top();
-                    stack.pop();
+                    if (!stack.empty())
+                    {
+                        std::cout << stack.top();
+                        stack.pop();
+                    }
                     break;
                 // Output top element as character
                 case ',':
-                    std::cout << (char)stack.top();
-                    stack.pop();
+                    if (!stack.empty())
+                    {
+                        std::cout << (char)stack.top();
+                        stack.pop();
+                    }
                     break;
                 
 // ######################################## Input ########################################
@@ -339,7 +492,10 @@ int main(int argc, char const *argv[])
 
                 // Stop the program
                 case '@':
-                    std::cout << "Program stopped" << std::endl;
+                    if (display_stopped)
+                    {
+                        std::cout << std::endl << "Program stopped" << std::endl;
+                    }
                     running = false;
                     break;
             }
@@ -348,6 +504,26 @@ int main(int argc, char const *argv[])
         // Move the cursor
         cursor_x += direction_x;
         cursor_y += direction_y;
+
+        // Wrap the cursor around the grid
+        if (cursor_x < 0)
+        {
+            cursor_x = COLS - 1;
+        }
+        else if (cursor_x >= COLS)
+        {
+            cursor_x = 0;
+        }
+
+        if (cursor_y < 0)
+        {
+            cursor_y = ROWS - 1;
+        }
+        else if (cursor_y >= ROWS)
+        {
+            cursor_y = 0;
+        }
+
     }
 
     if (running == true)
